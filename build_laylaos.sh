9ce80d38ee3c59c3efbc85bd854f3b8d4da88ad9
moisam <34644392+moisam@users.noreply.github.com>
2024-06-13 21:42:41 +0100

    Update build_laylaos.sh

--- build_laylaos.sh
+++ build_laylaos.sh
@@ -1,28 +1,22 @@
 #!/usr/bin/env bash
 
-if [ "${CROSS_BUILD_SYSROOT}x" == "x" ]; then
-    echo "Environment variable $$CROSS_BUILD_SYSROOT is not defined"
-    echo "Did you forget to 'source prep_cross.sh'?"
+if [ "${CROSSCOMPILE_SYSROOT_PATH}x" == "x" ]; then
+    echo "$0: Environment variable \$CROSSCOMPILE_SYSROOT_PATH is not defined"
+    echo "$0: Bailing out"
     exit 1
 fi
 
-if [ "${CROSS_BUILD_ARCH}" == "x86_64" ]; then
-    HOST="x86-64"
-else
-    HOST=${ARCH}
-fi
-
-cd src && make clean && make -j4 HOST=${HOST} && cp ./sdl2-doom ../sdl2-doom
-make HOST=${HOST} install
+cd src && make clean && make -j4 && cp ./sdl2-doom ../sdl2-doom
+make install
 cd ..
 
 # copy the icon
-mkdir -p ${CROSS_BUILD_SYSROOT}/usr/local/share/gui/icons/
-cp doom.ico ${CROSS_BUILD_SYSROOT}/usr/local/share/gui/icons/
+mkdir -p ${CROSSCOMPILE_SYSROOT_PATH}/usr/share/gui/icons/
+cp doom.ico ${CROSSCOMPILE_SYSROOT_PATH}/usr/share/gui/icons/
 
 # create a desktop entry file
-mkdir -p ${CROSS_BUILD_SYSROOT}/usr/local/share/gui/desktop/
-cat > ${CROSS_BUILD_SYSROOT}/usr/local/share/gui/desktop/doom.entry << EOF
+mkdir -p ${CROSSCOMPILE_SYSROOT_PATH}/usr/share/gui/desktop/
+cat > ${CROSSCOMPILE_SYSROOT_PATH}/usr/share/gui/desktop/doom.entry << EOF
 #
 # Desktop entry file
 #
